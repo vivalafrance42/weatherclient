46f6b5e8214500c8b5d98c048cd5c879f40f8c8e
blafrance42 <benlafrance42@gmail.com>
2024-02-23 20:41:44 -0800

    Added nav-bar, countries, & routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponent } from "./hello/hello.component";
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 
 
@@ -12,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HelloComponent, HttpClientModule]
+    imports: [RouterOutlet, HelloComponent, HttpClientModule, NavBarComponent]
 })
 export class AppComponent  {
 
